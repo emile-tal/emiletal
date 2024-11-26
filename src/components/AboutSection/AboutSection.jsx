2acc4cd96a84499dc2528fd15f52e52f2221f805
Emile Tal <emiletal@gmail.com>
2024-11-26 13:24:06 -0500

    Change favicon

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -9,7 +9,7 @@ export function AboutSection() {
             <div className='about__container'>
                 <div className='about__text-container'>
                     <p className='about__content'>
-                        I was first exposed to coding during my bachelor’s at the Ecole hoteliere de Lausanne. I enjoyed my “Intro to Python” course so much that on the side of my studies, I began learning basic HTML, CSS, and Javascript using online tools. After my bachelor’s, I realized that there was nothing I wanted more than to become a software engineer, and to keep learning how to code. I therefore pivoted into an immersive program with BrainStation where I developed the skills I needed to start my new career.
+                        I was first exposed to coding during my bachelor’s at the Ecole hoteliere de Lausanne. After thoroughly enjoying the 'Intro to Python' course, I decided to deepen my programming knowledge by learning basic HTML, CSS, and JavaScript online. After my bachelor’s, I realized that there was nothing I wanted more than to become a software engineer, and to keep learning how to code. I therefore pivoted into an immersive program with BrainStation where I developed the skills I needed to start my new career.
                     </p>
                     <p className='about__content'>
                         Currently living in New York City, I come from a very multicultural background, having lived 10 years in Hong Kong and with French and Israeli parents. I speak English, French, and Hebrew fluently. In my free time, I love cooking, traveling, running, and reading. I ran my first marathon in November 2023 and am excited to run another soon!
