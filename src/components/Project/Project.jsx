1beb736377a54f013eaec26bd5abbe6266712f02
Emile Tal <emiletal@gmail.com>
2024-12-09 18:54:06 -0500

    Add Check-In project to projects page

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,9 +1,23 @@
 import './Project.scss'
 
-export function Project() {
+export function Project({ projectName, projectOneliner, projectDescription, projectSrc, projectLink, githubFrontEnd, githubBackEnd }) {
     return (
         <div className='project'>
-
+            <a className='project__image-link' href={projectLink}>
+                <img className='project__image' alt={`${projectName} descriptive image of website`} src={projectSrc} />
+            </a>
+            <div className='project__text-container'>
+                <a className='project__header-link' href={projectLink}>
+                    <h2 className='project__header'>{projectName}</h2>
+                </a>
+                <p className='project__content project__content--emphasize'>{projectOneliner}</p>
+                <p className='project__content'>{projectDescription}</p>
+                <div className='project__link-container'>
+                    <a className='project__link' href={projectLink}>Live Site</a>
+                    <a className='project__link' href={githubFrontEnd}>Front-End Repo</a>
+                    <a className='project__link' href={githubBackEnd}>Back-End Repo</a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ProjectsContainer/ProjectsContainer.jsx
+++ src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -1,11 +1,23 @@
 import './ProjectsContainer.scss'
 
 import { Project } from '../Project/Project'
+import projects from '../../assets/data/projects'
 
 export function ProjectsContainer() {
     return (
         <div className='projects-container'>
-            <Project />
+            {projects.map((project, index) => (
+                <Project
+                    key={index}
+                    projectName={project.name}
+                    projectOneliner={project.oneLiner}
+                    projectDescription={project.description}
+                    projectSrc={project.src}
+                    projectLink={project.link}
+                    githubFrontEnd={project.githubFront}
+                    githubBackEnd={project.githubBack}
+                />
+            ))}
         </div>
     )
 }
